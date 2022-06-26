acb3722a1e8eba02de221be6e5542b7f6ac0b448
awp-finanznachrichten <sw@awp.ch>
2022-06-26 09:35:58 +0200

    commit from Rstudio

--- ranking_update.R
+++ ranking_update.R
@@ -16,15 +16,15 @@ source("ranking_funktionen.R",encoding = "UTF-8")
 
 repeat {
 Sys.sleep(10)
+
 #Check Update Time
 driver <- RSelenium::rsDriver(port= 4568L, browser = "firefox")
 remote_driver <- driver[["client"]]
   
 current_date <- Sys.Date()
-current_day <- as.numeric(format(Sys.Date(),"%d"))-1
+current_day <- as.numeric(format(Sys.Date(),"%d"))
 
 remote_driver$navigate("https://kassiesa.net/uefa/data/method5/crank2023.html")
-
 output <- remote_driver$findElement(using="class",value="flex-container")
 text_all <- output$getElementText()
 
@@ -37,21 +37,29 @@ remote_driver$close()
 #Close server
 driver[["server"]]$stop()
 
+#Stop Geckodriver
+try(system("taskkill /F /IM geckodriver.exe"))
+
 #Stop Java-Process
-system("taskkill /F /IM java.exe")
+try(system("taskkill /F /IM java.exe"))
 
 if (day == current_day) {
 print("Aktuelle Daten gefunden")
 break
 }
-if (as.numeric(format(Sys.time(),"%H")) == 1) {
+
+hour_number <- as.numeric(format(Sys.time(),"%H"))
+if (hour_number == 0) {
+hour_number <- 24  
+}  
+
+if (hour_number < 22) {
 break
 print("Keine neuen Daten gefunden an dem aktuellen Tag")
 
 }  
 print("Noch keine aktuellen Daten gefunden")
 }
-
 
 #Update Ranking Data
 source("ranking_scraping.R",encoding = "UTF-8")
